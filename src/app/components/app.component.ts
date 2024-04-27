6344e943d042348d148bfbcfc56e6d16bf4f5cc3
Claudiu <clayarname4@gmail.com>
2024-04-28 00:00:55 +0300

    Create scrolling chat UI

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
@@ -11,19 +12,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'chat-replay-frontend';
-
+  emoteSet: string[] = ["13", "17"];
   comments: string[] = [];
   i: number = 0;
 
   constructor( ) { 
-    this.comments = ["53","58","123"];
+    this.comments = ["53","58","testi2ng 123 35727 tahhhhhh1hh2hhhhhhhhhhhh j2e j2 nrjkg1fn2rejgner e2wr m2ew rm2wek rew2mkr ewmrew2kl r1werfnsdafn2dsjk1vnfjwlfkbre ffe1r2er fnke1r fe2rf erf1 er1f2re ", "325", "^$363346", "646","8686"];
   }
 
   update() {
-    console.log(this.comments)
-    console.log(this.i);
     this.i++;
     this.comments.splice(0,0,this.i.toString());
-    if (this.comments.length > 10 ) this.comments.splice(this.comments.length-1,1);
+    if (this.comments.length > 100 ) this.comments.splice(this.comments.length-1,1);
   }
 }
