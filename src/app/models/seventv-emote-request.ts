3e2c1c61e110191a0db353dfef510768554e5a60
Claudiu <clayarname4@gmail.com>
2024-05-07 02:28:18 +0300

    Remove unnecesary request interface

--- src/app/models/seventv-emote-request.ts
+++ src/app/models/seventv-emote-request.ts
@@ -5,8 +5,3 @@ export interface SevenTvEmote {
     height?: number;
     data:{ host: {files:[{height: number, width: number}]}};
 }
-
-export interface SevenTvEmoteRequest {
-    id: string;
-    emote_set: {emotes: SevenTvEmote[]};
-}
