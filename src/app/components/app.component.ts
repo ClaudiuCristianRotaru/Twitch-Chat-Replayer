8c46455d5294dde431c836bcc799a222a92c6bfd
Claudiu <clayarname4@gmail.com>
2024-05-07 03:30:28 +0300

    Implement web worker

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -6,25 +6,29 @@ import { EmoteSetService } from '../services/emote-set.service';
 import { take } from 'rxjs';
 import { Emote } from '../models/emote';
 import { ChatReplayer } from '../logic/chat-replayer';
+import { Time } from '../logic/time';
 import { Message } from '../models/message';
+import { MatSliderModule } from '@angular/material/slider';
 import { FormsModule } from '@angular/forms';
+import { TwitchDataService } from '../services/twitch-data.service';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, EmotifyPipe, FormsModule],
+  imports: [CommonModule, RouterOutlet, EmotifyPipe, MatSliderModule, FormsModule],
   providers: [EmotifyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewChecked {
 
   readonly userColors: string[] = ['red', 'green', 'turquoise', 'pink', 'limegreen', 'orange', 'burlywood', 'lightgray'];
   readonly MAX_DISPLAYED_MESSAGES: number = 100;
 
   sliderValueMinutes: number = 20;
+  timerWorker: any;
   title = 'chat-replay-frontend';
   emoteSet: Emote[] = [];
   messages: Message[] = [];
@@ -32,15 +36,73 @@ export class AppComponent implements OnInit {
   replayer: ChatReplayer = new ChatReplayer();
   channel: string = "erobb221";
   date: string = "2024/4/18";
-  constructor(private emoteSetService: EmoteSetService) { }
+
+  constructor(
+    private emoteSetService: EmoteSetService, 
+    private twitchDataService: TwitchDataService
+  ) { }
 
   ngOnInit(): void {
     console.log("ngOnInit() called.")
+    this.scrollToBottom();
+    this.twitchDataService.getChannelLogs(this.channel, this.date).pipe(take(1)).subscribe(response => {
+      let splitRequest: string[] = response.split("\r\n");
+      if (typeof Worker !== 'undefined') {
+        console.log("Trying to init worker");
+        this.initWorker(splitRequest);
+      }
+      else {
+        console.log("Worker is not supported");
+      }
+    })
     this.fetchEmoteSet();
+  }
+
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
   }
 
   placeholderClick() {
     console.log('update');
+  }
+
+  initWorker(msgs: string[]) {
+    this.timerWorker = new Worker(new URL('./timer.worker.ts', import.meta.url));
+    this.timerWorker.onmessage = (msg: any) => {
+      if (msg.data.status == 'finished') {
+        this.addMessage(new Message("", new Time(), "", "SERVER", msg.data.content));
+        return;
+      }
+      let messageData = msg.data.content;
+      let messageTime: Time = new Time(
+        messageData.time.hours,
+        messageData.time.minutes,
+        messageData.time.seconds,
+        messageData.time.milliseconds);
+      this.sliderValueMinutes = messageTime.hours * 60 + messageTime.minutes;
+      let message: Message = new Message(
+        messageData.date,
+        messageTime,
+        messageData.channel,
+        messageData.author,
+        messageData.content);
+      this.addMessage(message);
+    }
+    this.timerWorker.postMessage({ messages: msgs, time: new Time(22, 59, 50) })
+  }
+
+  updateCurrentTime(): void {
+    let t: Time = new Time();
+    t = t.convertMsToTime(this.sliderValueMinutes * 60 * 1000);
+    console.log(t);
+    this.addMessage(new Message("", new Time(), "", "SERVER", `Showing messages from: ${t.toShortString()}`));
+    this.timerWorker.postMessage({ time: t })
+  }
+
+  formatSliderTimeLabel(value: number): string {
+    let t = new Time();
+    t = t.convertMsToTime(value * 60 * 1000);
+    return t.toShortString();
   }
 
   addMessage(message: Message): void {
@@ -66,4 +128,10 @@ export class AppComponent implements OnInit {
     });
   }
 
+  @ViewChild('scrollMe') private myScrollContainer!: ElementRef;
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) { }
+  }
 }
