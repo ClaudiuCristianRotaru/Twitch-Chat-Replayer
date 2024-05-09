747a831e590be66e05d0c5e207261d877078ff49
Claudiu <clayarname4@gmail.com>
2024-05-10 01:00:10 +0300

    Create twitch data service

--- src/app/services/twitch-data.service.ts
+++ src/app/services/twitch-data.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
+import { CLIENT_ID, CLIENT_SECRET } from '../config'
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,17 @@ export class TwitchDataService {
 
   getChannelLogs(channel: string, date:string ): Observable<string> {
     return this.http.get(`https://logs.ivr.fi/channel/${channel}/${date}`, {responseType: 'text'}).pipe(map( r => r))
+  }
+
+  getTwitchOAuth(): Observable<any> {
+    const myheader = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+
+    let body = new HttpParams();
+    
+    body = body.set('client_id', CLIENT_ID);
+    body = body.set('client_secret', CLIENT_SECRET);
+    body = body.set('grant_type', 'client_credentials');
+    
+    return this.http.post<any>('https://id.twitch.tv/oauth2/token', body, {headers:myheader});
   }
 }
